d1d8421261acdeccbd1879cecc5330c114eb598e
Ajinkya <ajinkyad@winsoftech.com>
2018-09-29 18:34:51 +0530

    one way jorney fourm - html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,17 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatMomentDateModule } from '@angular/material-moment-adapter'
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { Moment } from 'moment';
+import * as moment from 'moment';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -20,7 +31,17 @@ import { OneWayComponent } from './components/search/one-way/one-way.component';
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatTabsModule
+    MatTabsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatMomentDateModule,
+    MatDatepickerModule,
+    MatSliderModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/search/one-way/one-way.component.ts
+++ src/app/components/search/one-way/one-way.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { coerceNumberProperty } from '@angular/cdk/coercion';
 
 @Component({
   selector: 'app-one-way',
@@ -7,7 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OneWayComponent implements OnInit {
 
-  constructor() { }
+  public originCityControl: FormControl;
+  public destinationCityControl: FormControl;
+  public noOfPersonControl: FormControl;
+  options: string[] = ['One', 'Two', 'Three'];
+
+  autoTicks = false;
+  disabled = false;
+  invert = false;
+  max = 10;
+  min = 1;
+  showTicks = false;
+  step = 1;
+  thumbLabel = true;
+  value = 1;
+  vertical = false;
+
+ get tickInterval(): number | 'auto' {
+   return this.showTicks ? (this.autoTicks ? 'auto' : this._tickInterval) : 0;
+ }
+ set tickInterval(value) {
+   this._tickInterval = coerceNumberProperty(value);
+ }
+ private _tickInterval = 1;
+
+  constructor() {
+    this.originCityControl = new FormControl();
+    this.destinationCityControl = new FormControl();
+    this.noOfPersonControl = new FormControl();
+  }
 
   ngOnInit() {
   }
